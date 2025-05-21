63abd5908d9e99c2bee7589eef6fbffbc83bd0a9
Krishna-mudgal <tonystark00127@gmail.com>
2025-05-21 23:13:13 +0530

    add one more solution of lc_73 to the repository

--- lc_73_set_matrix_zeroes.cpp
+++ lc_73_set_matrix_zeroes.cpp
@@ -34,3 +34,24 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+
+        vector<pair<int, int>> v;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0) v.push_back({i, j});
+            }
+        }
+
+        for(auto p : v){
+            int row = p.first, col = p.second;
+            for(int i = 0; i < n; i++) matrix[row][i] = 0;
+            for(int i = 0; i < m; i++) matrix[i][col] = 0;
+        }
+
+    }
+};
