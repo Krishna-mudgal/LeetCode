06afde2c6caf88f47f5dad0da22c7858ab10bc6d
Krishna-mudgal <tonystark00127@gmail.com>
2025-12-12 16:38:10 +0530

    add solution of lc_3531 to the repository

---
+++ lc_3531_count_covered_buildings.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/count-covered-buildings/description/?envType=daily-question&envId=2025-12-11
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        unordered_map<int, int> mp1, mp2, mp3, mp4;
+        sort(buildings.begin(), buildings.end());
+
+        for(int i = 0; i < buildings.size(); i++) {
+            if(mp1.find(buildings[i][0]) != mp1.end()) mp1[buildings[i][0]] = max(mp1[buildings[i][0]], buildings[i][1]);
+            else mp1[buildings[i][0]] = buildings[i][1];
+
+            if(mp2.find(buildings[i][1]) != mp2.end()) mp2[buildings[i][1]] = max(mp2[buildings[i][1]], buildings[i][0]);
+            else mp2[buildings[i][1]] = buildings[i][0];
+
+            if(mp3.find(buildings[i][0]) != mp3.end()) mp3[buildings[i][0]] = min(mp3[buildings[i][0]], buildings[i][1]);
+            else mp3[buildings[i][0]] = buildings[i][1];
+
+            if(mp4.find(buildings[i][1]) != mp4.end()) mp4[buildings[i][1]] = min(mp4[buildings[i][1]], buildings[i][0]);
+            else mp4[buildings[i][1]] = buildings[i][0];
+        }
+
+        int ans = 0;
+        bool flag = true;
+        for(auto &p : buildings) {
+            flag = true;
+            int x = p[0], y = p[1];
+            if(mp1[x] != mp3[x]) {
+                if(y < mp1[x] && y > mp3[x]) flag = false;
+            }
+
+            if(flag) continue;
+
+            if(mp2[y] != mp4[y]) {
+                if(x < mp2[y] && x > mp4[y]) ans++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+
+        unordered_map<int, vector<int>> mp1, mp2;
+        unordered_map<long long, int> flag;
+
+        auto key = [&](int x, int y) {
+            return (long long)x << 32 | y;
+        };
+
+        sort(buildings.begin(), buildings.end());
+
+        for (auto &b : buildings) {
+            mp1[b[0]].push_back(b[1]);
+            mp2[b[1]].push_back(b[0]);
+        }
+
+        // process rows
+        for (auto &p : mp1) {
+            auto &v = p.second;
+            sort(v.begin(), v.end());
+            if (v.size() > 2) {
+                for (int i = 1; i < v.size() - 1; i++)
+                    flag[key(p.first, v[i])]++;  
+            }
+        }
+
+        // process columns
+        for (auto &p : mp2) {
+            auto &v = p.second;
+            sort(v.begin(), v.end());
+            if (v.size() > 2) {
+                for (int i = 1; i < v.size() - 1; i++)
+                    flag[key(v[i], p.first)]++;  
+            }
+        }
+
+        int ans = 0;
+        for (auto &kv : flag) {
+            if (kv.second == 2) ans++;  // covered from both row + column
+        }
+
+        return ans;
+    }
+};
